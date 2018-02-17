975e3eae9f65dfbbf2414fd939335dee428c8d06
Zach Stevens <zachstevens39@gmail.com>
2018-02-17 15:08:08 -0700

    added more pixels for a total of 1,800

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // let canv = document.getElementById('canvas')
   let canvs = document.getElementsByClassName('canvas')
   let bruss = document.getElementsByClassName('brusher')
-  // let brus = document.getElementById('brusher').style.backgroundColor
   let whichBrush = ''
+  let isClick = false
   for (let i = 0; i < bruss.length; i++) {
     if (bruss[i].classList.contains('selected')) {
       whichBrush = bruss[i].classList.item(1)
-      // console.log('whichBrush is ', whichBrush)
     }
   }
-  // let canv = canvs[0];
-  // console.log('start??')
   function toggleBrush() {
     let colo = this.classList.item(1)
     if (whichBrush !== colo) {
@@ -20,39 +16,36 @@ document.addEventListener('DOMContentLoaded', function () {
       }
       this.classList.add('selected')
       whichBrush = colo
-      // console.log(whichBrush, ' selected')
     }
   }
-  function toggleRed() {
-    if (this.classList.contains('white')) {
-      this.classList.replace('white', 'red')
-      // console.log('is now red')
-    }
-    else {
-      this.classList.replace('red', 'white')
-      // console.log('is now white')
-    }
-  }
-  function toggleColor() {
+  function toggleColor() { // depricated
     if (!this.classList.contains(whichBrush)) {
       this.classList.replace(this.classList.item(1), whichBrush)
-      // console.log('is now ', whichBrush)
     }
   }
+  function toggleColorDown() {
+    isClick = true;
+    if (!this.classList.contains(whichBrush)) {
+      this.classList.replace(this.classList.item(1), whichBrush)
+    }
+  }
+  function toggleColorEnter() {
+    if (isClick) {
+      if (!this.classList.contains(whichBrush)) {
+        this.classList.replace(this.classList.item(1), whichBrush)
+      }
+    }
+  }
+  function toggleColorUp() {
+    isClick = false;
+  }
   for (let i = 0; i < canvs.length; i++) {
-    canvs[i].addEventListener('click', toggleColor)
+    // canvs[i].addEventListener('click', toggleColor)
+    canvs[i].addEventListener('mousedown', toggleColorDown)
+    canvs[i].addEventListener('mouseenter', toggleColorEnter)
+    canvs[i].addEventListener('mouseup', toggleColorUp)
   }
   for (let i = 0; i < bruss.length; i++) {
     bruss[i].addEventListener('click', toggleBrush)
   }
-  // if (canv.getAttribute('col').className === 'white') {
-  //   canv.setAttribute('col', 'red')
-  //   console.log('turned red')
-  // }
-  // else {
-  //   canv.setAttribute('col', 'white')
-  //   console.log('turned white')
-  // }
-  //
-  // canv.addEventListener('click', toggleColor)
 })
